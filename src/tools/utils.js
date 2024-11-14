4c9c6445514141ff8e54ce8eba615ecfc6d1773b
Eli Klein <eliklein02@gmail.com>
2024-11-14 16:48:02 -0500

    hi

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -1,5 +1,6 @@
 // const navigate = useNavigate();
 export const fetchUser = async (token) => {
+  console.log(token);
   if (!token) {
     return "not logged in";
   } else {
@@ -7,12 +8,19 @@ export const fetchUser = async (token) => {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/secret`, {
         headers: {
           Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "ngrok-skip-browser-warning": "69420",
         },
       });
+      if (response.ok) {
+        console.log("oksy");
+      }
+      console.log(response);
       const data = await response.json();
+      console.log(data);
       return data;
     } catch (error) {
-      window.location.href = "/server_error";
+      // window.location.href = "/server_error";
       console.log(error);
     }
   }
